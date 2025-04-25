5f40794d8ff5c0d1d523bd1df3353c5526324af9
AnMk24 <anahit.91.mkrtchyan@gmail.com>
2025-04-25 17:00:15 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+// window.onscroll = function() {stickyHeader()};
+
+// var header = document.querySelector(".site-header");
+// var sticky = header.offsetTop;
+
+// function stickyHeader() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+
+
+// document.querySelector("form").addEventListener("submit", function(e) {
+//   var name = document.querySelector("input[name='name']").value;
+//   var email = document.querySelector("input[name='email']").value;
+//   var valid = true;
+
+//   if (name === "" || email === "") {
+//     alert("Please fill in all fields.");
+//     valid = false;
+//   }
+
+
+
+//   if (!valid) {
+//     e.preventDefault();  
+//   }
+// });
+
+
+// var video = document.querySelector("video");
+// video.autoplay = true;
+// video.loop = true;
+// video.muted = true;
+
+
